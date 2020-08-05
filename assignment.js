f207d35b30a93b80c4b59498595110830770e239
pranak <pranakbarua111@gmail.com>
2020-08-05 19:19:57 +0600

    wood calculator done

--- assignment.js
+++ assignment.js
@@ -8,3 +8,20 @@ function feetToMile(feetValue){
 }
 var value=feetToMile(5280);
 console.log(value);
+
+
+
+
+//wood Calculator
+function woodCalculator(chair,table,bed){
+    if(chair<0 || table<0 || bed<0){
+        return "please enter positive value";
+    }
+    const chairs=chair*1;
+    const tables=table*2;
+    const beds=bed*5;
+    let total=chairs+tables+beds;
+    return total;
+}
+var woodValue=woodCalculator(5,2,3);
+console.log(woodValue);
